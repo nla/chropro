2a49b8c88f8c573c299431a3354743c2a0e25174
Alex Osborne <ato@meshy.org>
2016-11-10 00:11:52 +1100

    Add webapp

---
+++ webapp/src/chropro/Main.java
@@ -0,0 +1,57 @@
+package chropro;
+
+import org.imgscalr.Scalr;
+import spark.Spark;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+public class Main {
+    public static void main(String args[]) throws ExecutionException, InterruptedException, IOException {
+        Renderer renderer = new Renderer();
+        Spark.get("/image", (request, response) -> {
+            String url = request.queryParams("url");
+            Integer size = or(request.queryMap("size").integerValue(), 256);
+            Integer w = request.queryMap("w").integerValue();
+            Integer h = request.queryMap("h").integerValue();
+            Integer vpw = or(request.queryMap("vpw").integerValue(), 1280);
+            Integer vph = or(request.queryMap("vph").integerValue(), 800);
+            String format = or(request.queryParams("format"), "jpeg");
+            Integer timeout = or(request.queryMap("timeout").integerValue(), 10000);
+
+            if (url == null) {
+                response.status(400);
+                response.type("text/plain");
+                return "url is mandatory";
+            }
+
+            response.type("image/" + format);
+            try {
+                byte[] data = renderer.render(url, vpw, vph, timeout);
+                BufferedImage image = ImageIO.read(new ByteArrayInputStream(data));
+                if (w == null || h == null) {
+                    image = Scalr.resize(image, size);
+                } else {
+                    image = Scalr.resize(image, w, h);
+                }
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ImageIO.write(image, format, baos);
+                return baos.toByteArray();
+            } catch (TimeoutException e) {
+                response.status(500);
+                response.type("text/plain");
+                return "timed out";
+            }
+        });
+    }
+
+    private static <T> T or(T a, T b) {
+        return a == null ? b : a;
+    }
+
+}
---
+++ webapp/src/chropro/Renderer.java
@@ -0,0 +1,78 @@
+package chropro;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Base64;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+public class Renderer implements AutoCloseable {
+
+
+    private final Chropro chrome;
+    private final URI uri;
+
+    public Renderer() throws IOException {
+        this.uri = URI.create("ws://127.0.0.1:9292/devtools/browser");
+        this.chrome = new Chropro(uri);
+
+    }
+
+    public byte[] render(String url, int w, int h, int timeout) throws ExecutionException, InterruptedException, IOException, TimeoutException {
+        String contextId = chrome.target.createBrowserContext().get(timeout, MILLISECONDS).browserContextId;
+        String targetId = null;
+        try {
+            targetId = chrome.target.createTarget("about:blank", w, h, contextId).get(timeout, MILLISECONDS).targetId;
+
+            // Chrome is buggy and won't let us connect unless we've refreshed the json endpoint
+            jsonEndpoint(uri).toURL().openStream().close();
+
+            try (Chropro tab = new Chropro(uri.resolve("/devtools/page/" + targetId))) {
+                tab.target.getTargetInfo(targetId).get(timeout, MILLISECONDS);
+                tab.page.enable().get(timeout, MILLISECONDS);
+                CompletableFuture<PageDomain.LoadEventFired> loaded = tab.page.onLoadEventFired();
+
+                System.out.println("navigating");
+                tab.page.navigate(url).get(timeout, MILLISECONDS);
+
+                loaded.get(timeout, MILLISECONDS);
+
+                // disable scrollbars
+                tab.runtime.evaluate("document.getElementsByTagName('body')[0].style.overflow='hidden'", null, null, null, null, null, null, null, null).get(timeout, MILLISECONDS);
+
+                // wait a little for any onload javascript to fire
+                Thread.sleep(100);
+
+                String data = tab.page.captureScreenshot().get(timeout, MILLISECONDS).data;
+                return Base64.getDecoder().decode(data);
+            }
+        } finally {
+            if (targetId != null) {
+                chrome.target.closeTarget(targetId).get(timeout, MILLISECONDS);
+            }
+            chrome.target.disposeBrowserContext(contextId).get(timeout, MILLISECONDS);
+        }
+    }
+
+    /**
+     * "ws://127.0.0.1:9292/devtools/browser" => "http://127.0.0.1:9292/json"
+     */
+    static URI jsonEndpoint(URI uri) {
+        try {
+            return new URI("http",
+                    uri.getUserInfo(), uri.getHost(), uri.getPort(),
+                    "/json", null, null);
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        chrome.close();
+    }
+}
